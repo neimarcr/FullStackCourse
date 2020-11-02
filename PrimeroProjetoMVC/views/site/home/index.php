b52f160fd586de2a29483beadc79b3dbe07cc1cd
Neimar <neimar_cr@hotmail.com.br>
2020-11-02 19:56:02 -0300

    adicionado um tema, modificadas as imagens da página home e adicionado um texto

--- PrimeroProjetoMVC/views/site/home/index.php
+++ PrimeroProjetoMVC/views/site/home/index.php
@@ -4,13 +4,13 @@
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="assets/img/por_do_sol.jpg" class="d-block w-100" alt="...">
+          <img src="assets/img/aviao1.jpg" class="d-block w-100" style="height: 500px" alt="aviao">
         </div>
         <div class="carousel-item">
-          <img src="assets/img/por_do_sol_2.jpg" class="d-block w-100" alt="...">
+          <img src="assets/img/aviao2.jpg" class="d-block w-100" style="height: 500px" alt="...">
         </div>
         <div class="carousel-item">
-          <img src="assets/img/por_do_sol_3.jpg" class="d-block w-100" alt="...">
+          <img src="assets/img/aviao3.jpg" class="d-block w-100" style="height: 500px" alt="...">
           <!-- <img src="..." class="d-block w-100" alt="..."> -->
         </div>
       </div>
@@ -25,33 +25,42 @@
     </div>
   </section>
   <section class="section2 my-5">
-    <h3 class="mb-3">Section 2</h3>
+    <h3 class="mb-3">Destinos populares</h3>
     <div class="container">
       <div class="row">
-        <div class="col-12 col-md-4 col-lg-4 col-xl-4 mb-5 mb-md-0 mb-lg-0 mb-xl-0"><div class="imagem imagem1"></div></div>
-        <div class="col-12 col-md-4 col-lg-4 col-xl-4 mb-5 mb-md-0 mb-lg-0 mb-xl-0"><div class="imagem imagem2"></div></div>
-        <div class="col-12 col-md-4 col-lg-4 col-xl-4"><div class="imagem imagem3"></div></div>
+        <div class="col-12 col-md-4 col-lg-4 col-xl-4 mb-5 mb-md-0 mb-lg-0 mb-xl-0 image-conteiner">
+          <div class="imagem imagem1">
+            <h3>Paris</h3>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 col-xl-4 mb-5 mb-md-0 mb-lg-0 mb-xl-0 image-conteiner">
+          <div class="imagem imagem2">
+            <h3>Toquio</h3>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 col-xl-4 image-conteiner">
+          <div class="imagem imagem3">
+            <h3>Machu Picchu</h3>
+          </div>
+        </div>
       </div>
     </div>
   </section>
   <section class="section3">
-    <h3 class="mb-3">Section 3</h3>
+    <h3 class="mb-3">Por que viajar?</h3>
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-10">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Nos dias de hoje, temos cada vez mais responsabilidades e levamos rotinas cada vez mais agitadas, 
+            com diversos deveres e responsabilidades. Uma das melhores formas de podermos relaxar,
+             viver coisas novas e sairmos um pouco da rotina, é fazendo uma viagem. </p><br>
 
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Viajar também melhora a nossa saúde, além de toda a aprendizagem que temos ao conhecer novas culturas,
+            novas maneiras de pensar o mundo, o que pode nos ajudar em diversas áreas da nossa vida, como carreira, 
+            relacionamentos, comunicação, etc.
+          </p><br>
+          <p>Se você está buscando por novos horizontes e novas experiências, o que você está esperando, compre já a sua passagem!
+          </p>
         </div>
       <div>
     </div>
